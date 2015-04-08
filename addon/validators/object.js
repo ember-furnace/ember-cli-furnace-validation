f8b56aa3b1cd6873eca830499ef7319d9d5062fc
Eric van Blokland <eric@footsteps.nl>
2015-04-08 15:36:28 +0200

    Fixed issue with recursion detection. Different object validators may now validate the same object.

--- addon/validators/object.js
+++ addon/validators/object.js
@@ -1,4 +1,5 @@
 import Promise from './promise';
+import Abstract from './abstract';
 import Ember from 'ember';
 
 /**
@@ -34,7 +35,7 @@ export default Promise.extend({
 	validators: Ember.computed(function() {
 		var ret = {};
 		var self = this;
-		this.constructor.eachComputedProperty(function(name, meta) {			
+		this.constructor.eachComputedProperty(function(name, meta) {
 			if (meta.type==='validation') {
 				ret[name]=self.get(name);
 			}
@@ -50,15 +51,23 @@ export default Promise.extend({
 	init: function() {
 		this._super();	
 		this._validators={};
-		this.get('validators');
+		// If we have recursive validators, getting the validators on init will cause infinite recursion
+		// Do we really need to get them all on init?
+		//this.get('validators');
 	},
 	
 	_validate: function(context) {		
 		var promises=Ember.A();
 		Ember.assert('The object validator can\'t validate enumerables',!Ember.Enumerable.detect(context.value));
+		
 		// Check the stack for circular checks
-		if(!context.stack.contains(context.value)) {
-			context.stack.push(context.value);
+		// - State-validators may run different object validators on the same object. 
+		// - Additionally, a nested object may run a different object check on a related parent object.
+		// - Finally, we could do different object based validations on the same property
+		// So: check both the value as the validator instance in the stack
+		if(context.stack.filterBy('value',context.value).filterBy('validator',this).length===0) {
+			context.stack.push({value:context.value,
+								validator:this});
 			var validators=this.get('validators');
 			for(var propertyName in validators) {
 				var nestedContext=context.nest(propertyName);
