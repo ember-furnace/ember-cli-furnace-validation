606ae1f9e6ef776817bf58a55348f44bfd3e7bd0
Eric van Blokland <eric@footsteps.nl>
2015-07-14 10:23:58 +0200

    Added flags to required validator to accept NULL or FALSE values.

--- app/validators/local/required.js
+++ app/validators/local/required.js
@@ -4,8 +4,11 @@ export default Validator.extend({
 	
 	messages : {blank : 'blank' },
 	
+	allowFalse : false,
+	allowNull : false,
+	
 	call : function(context,value,result) {
-		if(Ember.isBlank(value) || value===false) {			
+		if((Ember.isBlank(value) && !(this.allowNull===true && value===null)) || (this.allowFalse===false && value===false)) {			
 			result.addError(context,this.messages.blank,null,'blur');
 		}
 	}
