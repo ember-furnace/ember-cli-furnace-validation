e3fb33c31de1aca037214f7c01cb26b62cf6ed8c
Eric van Blokland <eric@footsteps.nl>
2015-11-02 14:56:06 +0100

    Fixed queue skipping already queued validation

--- addon/observer-queue.js
+++ addon/observer-queue.js
@@ -17,14 +17,22 @@ export default Ember.Object.extend({
 	
 	push: function(validator,context,sender) {
 		this.length++;
+		var skip=false;
 		this._queue.forEach(function(queued) {
 			if(queued.validator===validator && queued.context.path===context.path) {
-				Ember.warn('Skipping already queued path, this has not been tested');
+				if(this._debugLogging) {
+					this._logEvent(this+': Skipping',validator.toString(),context.path);
+				}	
+				skip=true;
 				return;
 			}
-		});
+		},this);
+		if(skip) {
+			this.length--;
+			return;
+		}
 		if(this._debugLogging) {
-			this._logEvent('New in queue ('+this.length+')',validator.toString(),context.path);
+			this._logEvent(this+': New in queue ('+this.length+')',validator.toString(),context.path);
 		}
 		this._queue.push({validator : validator, context: context,sender: sender});
 	},
@@ -39,23 +47,26 @@ export default Ember.Object.extend({
 		}
 		this._running=true;
 		var queue=this;
-		var next=this._queue.shift();
+		var next=this._queue[0];
 		if(next) {
-			
+			if(queue._debugLogging) {
+				queue._logEvent(this+': Running ',next.validator.toString(),next.context.path);
+			}
 			next.context.result.reset(next.context,true);
 			next.context.resetStack();
 			next.validator._validate(next.context).then(function(result){
+				queue._queue.shift();
 				result.updateValidity(next.context,true);
 				queue.length--;
 				queue._running=false;
 				if(queue.length) {
 					if(queue._debugLogging) {
-						queue._logEvent('Next in queue',queue._queue.get('firstObject').sender.toString(),queue._queue.get('firstObject').context.path);
+						queue._logEvent(queue+': Next in queue',queue._queue.get('firstObject').validator.toString(),queue._queue.get('firstObject').context.path);
 					}
 					queue.run();
 				}else {
 					if(queue._debugLogging) {
-						queue._logEvent('Queue clear, running callback',next.sender.toString(),next.context.path);
+						queue._logEvent(queue+': Queue clear, running callback',next.sender.toString(),next.context.path);
 					}
 					queue.callback(result,next.sender,next.context.key);
 				}
